5fe8255ba0fa33c1d7e2434d532815275e066cfe
mygardiner <143014738+mygardiner@users.noreply.github.com>
2024-04-30 22:28:45 -0700

    Update LightBoard.java

--- src/main/java/LightBoard.java
+++ src/main/java/LightBoard.java
@@ -28,13 +28,13 @@ public class LightBoard
     int numOn = 0;
     for (int r = 0; r < lights.length; r++){
       if (lights[r][col]){
-        numOn;
+        numOn++;
       }
     }
-    if (lights[row][col] && numOn % 2 ==0){
+    if (lights[row][col] && numOn % 2 == 0){
       return false;
     }
-    if (!lights[row][col] && numOn % 3 ==0){
+    if (!lights[row][col] && numOn % 3 == 0){
       return true;
     }
     return lights[row][col];
